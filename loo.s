ce5d31bc30f3486ea8b533d420fccafa266af72f
Suplementtipulma <koaguloitunut@gmail.com>
2016-11-18 01:48:11 +0200

    Hash function continued & code cleaned

--- loo.s
+++ loo.s
@@ -32,36 +32,19 @@ fileHandle:	.long 0
 .equ		BITMAPWIDTHOFFSET,  18	#4 bytes
 .equ		BITMAPHEIGHTOFFSET, 22	#4 bytes
 .equ		BITSPERPIXELOFFSET, 28  #2 bytes	
-<<<<<<< HEAD
 .equ		COMPRESSUSEDOFFSET, 30  #4 bytes	tulee olla BL_RGB eli 0				
 .equ		IMAGESIZEOFFSET,    34	#4 bytes
 
-=======
-.equ		COMPRESSUSEDOFFSET, 30  #4 bytes	 tulee olla BL_RGB eli 0				
-.equ		IMAGESIZEOFFSET,    34	#4 bytes	
-	
->>>>>>> 46ea300d9b752a0eabd3c390125068adf9ab3384
 #.equ		FIRSTLOC, /*...*/
 #.equ		OFFSETHOR, /*...*/
 #.equ		OFFSETVER, /*...*/
 	
 bmpID:		.ascii "BM"
 msg:		.ascii "\n>\0"
-<<<<<<< HEAD
 errorMsg:	.ascii "Error: Invalid file format\0"
 license:	.ascii "loo  Copyright (C) 2016  Suplementtipulma\nThis program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions; type `show c' for details.\n\n\0"
 
 primeConstant:	   .int 191
-=======
-	
-#dataRaw:	.ascii "FY10\0" "HI7\0" "PS7\0"
-#		       "MU5\0" "BI6\0" "BI6\0" "ÄI83\0"
-#		       "ÄI12\0" "ENA103\0" "RAA9+RA\0"
-#		       "TE3\0" "UE31\0" "MAA142\0" "MU9\0"
-#		       "KE3\0" "YH8\0" "ESB10\0" "MAA141\0" "SAA9+SA\0"
-data:		   .fill 125
-dataSize:	   .int 19
->>>>>>> 46ea300d9b752a0eabd3c390125068adf9ab3384
 userInputSize:	   .int 0
 userInputElements: .int 0	
 
@@ -72,10 +55,10 @@ dataRaw:
 	#tilaa 100 eri aineelle, joissa enint. 8 kirjainta
 	
 	.rept 100
-	.ascii "\0\0\0\0\0\0\0\0\0"				#Ensimmäinen tavu kertoo kyseisen aineen sijaintikolmikon sijainnin taulukossa
+	.ascii "\0\0\0\0\0\0\0\0\0"				#Ensimm�inen tavu kertoo kyseisen aineen sijaintikolmikon sijainnin taulukossa
 	.endr
 	
-	#8 erilaista laatikkoa -> 8*3 sijaintia			sijainnit kolmen ryhmissä eli 8 eri ryhmää
+	#8 erilaista laatikkoa -> 8*3 sijaintia			sijainnit kolmen ryhmiss� eli 8 eri ryhm��
 	
 	.rept 24
 	.byte 0
@@ -268,175 +251,8 @@ hashLoop:
 	
 	jne hashLoop
 	
-<<<<<<< HEAD
 hashEnd:
 	movl $0, %edx
-=======
-hashFind:	
-	
-	
-disp:	
-
-
-
-	
-
-	
-
-
-
- 
-	
-
-
-=======
-#loo 
-#Copyright (C) 2016  Suplementtipulma
-
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-#COMPILE 32-BIT gcc -m32 -o loo loo.s
-	
-.section .data
-filePath:	.ascii "./esimluk.bmp\0"
-readMode:	.ascii "r\0"
-fileHandle:	.long 0
-	
-.equ		NEWLINE, '\n'
-.equ		VARSIZE,  10
-.equ		NULLTERM,'\0'	
-#.equ		BMPIDENTIFIER, 'BM'
-bmpID:		.ascii "BM"
-errorMsg:	.ascii "Error: Invalid file format\0"
-license:	.ascii "loo  Copyright (C) 2016  suplementtipulma\nThis program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions; type `show c' for details.\n\n\0"
-
-.equ		STARTADDROFFSET,    10	#4 bytes
-.equ		BITMAPWIDTHOFFSET,  18	#4 bytes
-.equ		BITMAPHEIGHTOFFSET, 22	#4 bytes
-.equ		BITSPERPIXELOFFSET, 28  #2 bytes	
-.equ		COMPRESSUSEDOFFSET, 30  #4 bytes	tulee olla BL_RGB eli 0				
-.equ		IMAGESIZEOFFSET,    34	#4 bytes	
-	
-#.equ		FIRSTLOC, /*...*/
-#.equ		OFFSETHOR, /*...*/
-#.equ		OFFSETVER, /*...*/	
-	
-msg:		.ascii "\n>\0"
-	
-#dataRaw:	.ascii "FY10\0" "HI7\0" "PS7\0"
-#		       "MU5\0" "BI6\0" "BI6\0" "Ã„I83\0"
-#		       "Ã„I12\0" "ENA103\0" "RAA9+RA\0"
-#		       "TE3\0" "UE31\0" "MAA142\0" "MU9\0"
-#		       "KE3\0" "YH8\0" "ESB10\0" "MAA141\0" "SAA9+SA\0"
-data:		   .fill 125
-dataSize:	   .int 19
-userInputSize:	   .int 0
-userInputElements: .int 0	
-
-
-	
-readBuffer:	   .fill 10
-	
-#dataRaw:
-#	.ascii "FY10\0"
-#	.ascii "HI7\0"
-#	.ascii "PS7\0"
-#	.ascii "\0"
-#	.ascii "MU5\0"
-#	.ascii "BI6\0"
-#	.ascii "BI6\0"
-#	.ascii "AI83\0"
-#	.ascii "\0"
-#	.ascii "AI12\0"
-#	.ascii "ENA103\0"
-#	.ascii "RAA9+RA\0"
-#	.ascii "\0"
-#	.ascii "TE3\0"
-#	.ascii "UE31\0"
-#	.ascii "MAA142\0"
-#	.ascii "MU9\0"
-#	.ascii "\0"
-#	.ascii "KE3\0"
-#	.ascii "YH8\0"
-#	.ascii "ESB10\0"
-#	.ascii "MAA141\0"
-#	.ascii "SAA9+SA\0"
-#	#FY10 & HI7 & PS7 x  y	x  y 
-#	.byte 0, 0, 3, 3, 5, 4, 0, 0
-#	#MU5 & BI6 & BI6 & AI83
-#	.byte 0, 1,
-	
-dataRaw:	
-	#tilaa 100 eri aineelle, joissa enint. 8 kirjainta
-	
-	.rept 100
-	.ascii "\0\0\0\0\0\0\0\0\0"				#EnsimmÃ¤inen tavu kertoo kyseisen aineen sijaintikolmikon sijainnin taulukossa
-	.endr
-	
-	#8 erilaista laatikkoa -> 8*3 sijaintia			sijainnit kolmen ryhmissÃ¤ eli 8 eri ryhmÃ¤Ã¤
-	
-	.rept 24
-	.byte 0
-	.endr
-	
-	
-#hashedUserInput:
-#	.rept 10
-#	.int 0
-#	.endr
-
-.section .bss
-.lcomm userInput, 50
-.lcomm userInputLengths, 20	
-	
-.section .text
-
-	
-.globl _main	
-_main:
-	pushl $license
-	call _printf
-	addl $4, %esp
-
-	call init
-	call readInput
-	#call calc
-	jmp exit
-
-
-init:
-	pushl %ebp
-	movl %esp, %ebp
-
-	pushl $readMode
-	pushl $filePath
-	call _fopen
-	movl %eax, fileHandle(,1)
-	addl $8, %esp	
-
-	pushl $1
-	pushl $2
-	call readFromStream 
-	
-	movw readBuffer, %dx
-	cmpw bmpID, %dx  
-	jne ffError
-
-	pushl $STARTADDROFFSET
-	call setOffset
->>>>>>> 46ea300d9b752a0eabd3c390125068adf9ab3384
 	
 	movl $TABLESIZE, %ebx
 	divl %ebx
